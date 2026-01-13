99e6cf2fcd4e926c6ec1213d6adf3db4d6b8fa4c
paulinGAMEPRAY <paulingamepray1902@gmail.com>
2026-01-13 07:18:28 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>quantos pratos de trigo os 3 tigres tristes comeram?</h1>
+    return <h1>quantos pratos de trigo os 3 tigres tristes comeram ?</h1>
 }
 
 export default Home;
